22af2b5945d988dad6b1dd29fc6ffc36046599a5
mornir <jeromepott@hotmail.com>
2020-11-04 21:50:39 +0100

    Flatten structure

--- schemas/documents/deTerm.js
+++ schemas/documents/deTerm.js
@@ -3,16 +3,22 @@ export default {
   name: 'deTerm',
   type: 'document',
   liveEdit: true,
+  fieldsets: [
+    {
+      name: 'abbreviation',
+      title: 'Abkürzung (falls vorhanden)',
+      options: {
+        collapsible: true,
+        collapsed: true,
+      },
+    },
+  ],
   fields: [
     {
       type: 'string',
       name: 'term',
-      title: 'Begriff',
-    },
-    {
-      type: 'blockContent',
-      name: 'sourceTerm',
-      title: 'Quelle des Begriffs',
+      title: 'Benennung',
+      validation: (Rule) => Rule.required().error('Pflichtfeld'),
     },
     {
       type: 'string',
@@ -20,21 +26,51 @@ export default {
       title: 'Status',
       options: {
         list: [
-          { title: 'Hauptbegriff', value: 'hauptbegriff' },
           { title: 'genormt', value: 'genormt' },
           { title: 'nicht genormt', value: 'nicht_genormt' },
-          { title: 'veraltet', value: 'neu' },
+          { title: 'veraltet', value: 'old' },
           { title: 'abzulehnen', value: 'abzulehnen' },
           { title: 'zulässig', value: 'zulässig' },
         ],
       },
     },
     {
-      type: 'array',
-      name: 'additionnalFields',
-      title: 'Weitere Felder',
-      description: 'Anmerkung, Abkürzung, usw.',
-      of: [{ type: 'abbreviation' }, { type: 'notice' }],
+      type: 'blockContent',
+      name: 'sourceTerm',
+      title: 'Quelle der Benennung',
+    },
+    {
+      title: 'Anmerkung (fakultativ)',
+      name: 'notice',
+      type: 'blockContent',
+      description: 'Benennungsbezogenen oder begriffsbezogenen Informationen',
+    },
+    {
+      title: 'Benennung',
+      name: 'abbreviation',
+      type: 'string',
+      fieldset: 'abbreviation',
+    },
+    {
+      type: 'string',
+      name: 'abbreviationStatus',
+      title: 'Status',
+      fieldset: 'abbreviation',
+      options: {
+        list: [
+          { title: 'genormt', value: 'genormt' },
+          { title: 'nicht genormt', value: 'nicht_genormt' },
+          { title: 'veraltet', value: 'old' },
+          { title: 'abzulehnen', value: 'abzulehnen' },
+          { title: 'zulässig', value: 'zulässig' },
+        ],
+      },
+    },
+    {
+      title: 'Quelle',
+      name: 'abbreviationSource',
+      type: 'blockContent',
+      fieldset: 'abbreviation',
     },
   ],
   preview: {
