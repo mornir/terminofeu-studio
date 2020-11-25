4909430b30cd7caf16a20ece68a528bd0a1a1c86
mornir <jeromepott@hotmail.com>
2020-11-25 13:10:24 +0100

    Write value in English

--- schemas/builder/status.js
+++ schemas/builder/status.js
@@ -1,9 +1,9 @@
 const generateStatus = (type = 'begriff') => {
   const mainTitle = 'Haupt' + type
   return [
-    { title: mainTitle, value: mainTitle },
-    { title: 'Alternativ', value: 'Alternativ' },
-    { title: 'Abgelehnt', value: 'Abgelehnt' },
+    { title: mainTitle, value: 'main' },
+    { title: 'Alternativ', value: 'variant' },
+    { title: 'Abgelehnt', value: 'rejected' },
   ]
 }
 
