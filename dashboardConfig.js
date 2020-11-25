8c24ea2b5669d9d59452c0e9b1397254b9cfb5b2
mornir <jeromepott@hotmail.com>
2020-11-25 19:36:21 +0100

    Add document lists on dashboard

--- dashboardConfig.js
+++ dashboardConfig.js
@@ -1,6 +1,24 @@
 export default {
   widgets: [
     {
+      name: 'document-list',
+      options: {
+        title: 'Zuletzt bearbeitete Einträge',
+        order: '_updatedAt desc',
+        limit: 10,
+        types: ['entry'],
+      },
+    },
+    {
+      name: 'document-list',
+      options: {
+        title: 'Zuletzt bearbeitete Quellen',
+        order: '_updatedAt desc',
+        limit: 10,
+        types: ['source'],
+      },
+    },
+    {
       name: 'project-users',
     },
   ],
