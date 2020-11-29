c4bae4889014991e55dbb5a56db2e9629d5135c2
mornir <jeromepott@hotmail.com>
2020-11-29 08:27:42 +0100

    fix: show standard actions for other documents

--- resolveDocumentActions.js
+++ resolveDocumentActions.js
@@ -5,6 +5,10 @@ import defaultResolve, {
 import { setEntryTitlesAction } from './workflows/setEntryTitlesAction'
 
 export default function resolveDocumentActions(props) {
+  if (props.type !== 'entry') {
+    return defaultResolve(props)
+  }
+
   return defaultResolve(props).map((Action) =>
     Action === PublishAction ? setEntryTitlesAction : Action
   )
