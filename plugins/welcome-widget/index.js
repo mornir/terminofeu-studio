fdfa2abb54d8901c2bb1e3d186ba45867274bb0a
mornir <jeromepott@hotmail.com>
2021-02-18 15:06:43 +0100

    Use functional component

--- plugins/welcome-widget/index.js
+++ plugins/welcome-widget/index.js
@@ -1,49 +1,44 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
 import styles from './welcome.css'
 
-class Welcome extends React.Component {
-  render() {
-    return (
-      <div className={styles.container}>
-        <header className={styles.header}>
-          <h2 className={styles.title}>Willkommen zu Terminofeu</h2>
-          <h3 className={styles.subtitle}>
-            Begriffsmanagment für die BSV 2026
-          </h3>
-        </header>
-        <div className={styles.content}>
-          <a
-            className={styles.link}
-            rel="noopener nofollow"
-            target="_blank"
-            href="https://www.notion.so/terminofeu-Handbuch-e13f172c758b418c9e603d482462a58d"
-          >
-            Handbuch lesen
-          </a>
-          <span className={styles.separator}>|</span>
-          <a
-            className={styles.link}
-            rel="noopener nofollow"
-            target="_blank"
-            href="https://www.notion.so/Changelog-583f82bb03454ca591797d544e393544"
-          >
-            Changelog ansehen
-          </a>
-          <span className={styles.separator}>|</span>
-          <a
-            className={styles.link}
-            rel="noopener nofollow"
-            target="_blank"
-            href="https://terminofeu.ch"
-          >
-            terminofeu.ch öffnen
-          </a>
-        </div>
+function Welcome() {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h2 className={styles.title}>Willkommen zu Terminofeu</h2>
+        <h3 className={styles.subtitle}>Begriffsmanagment für die BSV 2026</h3>
+      </header>
+      <div className={styles.content}>
+        <a
+          className={styles.link}
+          rel="noopener nofollow"
+          target="_blank"
+          href="https://www.notion.so/terminofeu-Handbuch-e13f172c758b418c9e603d482462a58d"
+        >
+          Handbuch lesen
+        </a>
+        <span className={styles.separator}>|</span>
+        <a
+          className={styles.link}
+          rel="noopener nofollow"
+          target="_blank"
+          href="https://www.notion.so/Changelog-583f82bb03454ca591797d544e393544"
+        >
+          Changelog ansehen
+        </a>
+        <span className={styles.separator}>|</span>
+        <a
+          className={styles.link}
+          rel="noopener nofollow"
+          target="_blank"
+          href="https://terminofeu.ch"
+        >
+          terminofeu.ch öffnen
+        </a>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default {
