51f90593fbdc1e5f66c00c8c23a86613045f74f0
mornir <jeromepott@hotmail.com>
2021-02-19 11:01:08 +0100

    Code cleanup

--- plugins/entries-stats/MyTool.js
+++ plugins/entries-stats/MyTool.js
@@ -34,24 +34,6 @@ const query = `
    ${list}
 }`
 
-const data01 = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
-  { name: 'Group E', value: 278 },
-  { name: 'Group F', value: 189 },
-]
-
-const data02 = [
-  { name: 'Group A', value: 2400 },
-  { name: 'Group B', value: 4567 },
-  { name: 'Group C', value: 1398 },
-  { name: 'Group D', value: 9800 },
-  { name: 'Group E', value: 3908 },
-  { name: 'Group F', value: 4800 },
-]
-
 function MyTool() {
   const [data, setData] = useState(null)
 
@@ -75,7 +57,7 @@ function MyTool() {
 
   return (
     <div className={styles.container}>
-      <Heading as="h3" size={5} marginBottom={3} paddinLeft={4}>
+      <Heading as="h2" size={3} marginBottom={3} paddinLeft={4}>
         Einträgeanzahl je nach Status
       </Heading>
 
