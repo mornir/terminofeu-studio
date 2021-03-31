b172d7586f19bd594a2743a957524a3428f0e2b9
mornir <jeromepott@hotmail.com>
2021-03-31 17:06:36 +0200

    Disable sortering for definitions I think that this is not needed. I'll renable it if necessary.

--- schemas/builder/langFn.js
+++ schemas/builder/langFn.js
@@ -21,6 +21,9 @@ export default ({ title, code }) => {
         name: 'definitions',
         type: 'array',
         of: [{ type: 'definition' }],
+        options: {
+          sortable: false,
+        },
       },
     ],
   }
