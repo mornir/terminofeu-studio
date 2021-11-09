cb50f71fd5bad4320eab2da9ed3bc61bf84e3042
mornir <jeromepott@hotmail.com>
2021-11-09 13:27:01 +0100

    update: remove approval table

--- plugins/entries-stats/MyTool.js
+++ plugins/entries-stats/MyTool.js
@@ -1,10 +1,8 @@
-import React, { useEffect, useState, useMemo } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import sanityClient from 'part:@sanity/base/client'
 import { statusList } from '../../schemas/data/statusList'
 import { Heading, Box } from '@sanity/ui'
-
-import ApprovalTable from './ApprovalTable'
 
 import {
   BarChart,
@@ -14,8 +12,6 @@ import {
   Tooltip,
   Bar,
   ResponsiveContainer,
-  PieChart,
-  Pie,
 } from 'recharts'
 
 // Sanity uses CSS modules for styling. We import a stylesheet and get an
@@ -90,12 +86,12 @@ function MyTool() {
 
         <Tooltip />
       </PieChart> */}
-      <Box marginBottom={4} marginLeft={5} marginTop={5}>
+      {/*    <Box marginBottom={4} marginLeft={5} marginTop={5}>
         <Heading as="h2" size={2} style={{ textAlign: 'center' }}>
           Abstimmungen
         </Heading>
       </Box>
-      <ApprovalTable />
+      <ApprovalTable /> */}
     </div>
   )
 }
