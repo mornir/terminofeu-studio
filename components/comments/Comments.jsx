bcab7ec0be2ca7d8e0b6cb6714c63d8f9fe918e2
mornir <jeromepott@hotmail.com>
2022-03-30 14:25:20 +0200

    feat: better handle no comments case

--- components/comments/Comments.jsx
+++ components/comments/Comments.jsx
@@ -6,23 +6,33 @@ import styles from './Comments.css'
 function Comments({ document }) {
   const { displayed } = document
 
+  function checkNotes(notes) {
+    if (notes) {
+      return (
+        <Stack space={[3, 3, 4]} marginBottom={6}>
+          <Heading as="h2" size={2}>
+            Kommentare
+          </Heading>
+          {displayed.notes?.map((note) => {
+            return (
+              <Card padding={[3, 3, 4]} radius={2} shadow={1}>
+                <Box marginBottom={3}>
+                  <Label size={0}>{note.author}</Label>
+                </Box>
+                <p class={styles.noteText}>{note.text}</p>
+              </Card>
+            )
+          })}
+        </Stack>
+      )
+    } else {
+      return <p className={styles.noComment}>Keine Kommentare</p>
+    }
+  }
+
   return (
     <div className={styles.container} lang="de">
-      <Stack space={[3, 3, 4]} marginBottom={6}>
-        <Heading as="h2" size={2}>
-          Kommentare
-        </Heading>
-        {displayed.notes?.map((note) => {
-          return (
-            <Card padding={[3, 3, 4]} radius={2} shadow={1}>
-              <Box marginBottom={3}>
-                <Label size={0}>{note.author}</Label>
-              </Box>
-              <p class={styles.noteText}>{note.text}</p>
-            </Card>
-          )
-        })}
-      </Stack>
+      {checkNotes(displayed.notes)}
     </div>
   )
 }
