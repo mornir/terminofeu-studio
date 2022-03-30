e24ed5768d56338679f8dbf8234437bc8f19a465
mornir <jeromepott@hotmail.com>
2022-03-30 14:05:17 +0200

    Remove custom form closes #149

--- components/comments/Comments.jsx
+++ components/comments/Comments.jsx
@@ -1,37 +1,10 @@
-import React, { useState } from 'react'
-import { Stack, Card, TextArea, Button, Label, Box, Heading } from '@sanity/ui'
-import sanityClient from 'part:@sanity/base/client'
-import userStore from 'part:@sanity/base/user'
-import { nanoid } from 'nanoid'
+import React from 'react'
+import { Stack, Card, Label, Box, Heading } from '@sanity/ui'
 
 import styles from './Comments.css'
 
 function Comments({ document }) {
   const { displayed } = document
-
-  const [text, setText] = useState('')
-  const [isSending, setIsSending] = useState(false)
-  
-  async function postText(event) {
-    event.preventDefault()
-    if (isSending) return
-
-    setIsSending(true)
-
-    const { displayName } = await userStore.getUser('me')
-
-    await sanityClient
-      .patch(displayed._id)
-      .setIfMissing({ notes: [] })
-      .append('notes', [{ _key: nanoid(), author: displayName, text }])
-      .commit()
-      .catch((error) => {
-        console.error('Transaction failed: ', error.message)
-      })
-
-    setText('')
-    setIsSending(false)
-  }
 
   return (
     <div className={styles.container} lang="de">
@@ -50,28 +23,6 @@ function Comments({ document }) {
           )
         })}
       </Stack>
-
-      <form onSubmit={postText}>
-        <Stack space={[3, 3, 4]} marginBottom={2}>
-          <TextArea
-            fontSize={2}
-            padding={[3, 3, 4]}
-            value={text}
-            name="text"
-            onChange={(event) => setText(event.target.value)}
-            placeholder="Kommentar hier schreiben"
-            required
-          />
-          <Button
-            fontSize={[2, 2, 3]}
-            padding={[3, 3, 4]}
-            text="Kommentieren"
-            tone="primary"
-            type="submit"
-            disabled={isSending}
-          />
-        </Stack>
-      </form>
     </div>
   )
 }
