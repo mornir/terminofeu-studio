82d31c6230c265833cb1e8076fc1b62beb7a2d9b
mornir <jeromepott@hotmail.com>
2022-09-11 16:47:22 +0200

    feat: add possibility to assign entry to group

--- schemas/documents/entry.js
+++ schemas/documents/entry.js
@@ -44,6 +44,26 @@ export default {
       },
     },
     {
+      title: 'Zuständigkeit',
+      name: 'assignees',
+      type: 'array',
+      of: [{ type: 'string' }],
+      fieldset: 'admin',
+      options: {
+        list: [
+          { title: 'AG2', value: 'AG2' },
+          { title: 'AG3', value: 'AG3' },
+          { title: 'AG4', value: 'AG4' },
+          { title: 'AG5', value: 'AG5' },
+          { title: 'AG6', value: 'AG6' },
+          { title: 'AG7', value: 'AG7' },
+          { title: 'AG8', value: 'AG8' },
+        ],
+        layout: 'radio',
+        direction: 'horizontal',
+      },
+    },
+    {
       title: 'Übersetzungstatus',
       type: 'string',
       name: 'translationStatus',
