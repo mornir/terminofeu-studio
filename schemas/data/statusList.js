396f1bad39cb36b4aaaf82f01a5c6e4a9b5b972a
mornir <jeromepott@hotmail.com>
2023-02-28 08:36:42 +0100

    update: move category to end closes #185

--- schemas/data/statusList.js
+++ schemas/data/statusList.js
@@ -1,8 +1,8 @@
 export const statusList = [
-  { title: 'Nicht übernommen', value: 'draft' },
   { title: 'Entwurf', value: 'new_draft' },
   { title: 'Im Definitionsprozess', value: 'definition' },
   { title: 'Fachliche Freigabe', value: 'approved' },
   { title: 'Freigabe durch Kernausschuss', value: 'validated' },
   { title: 'Übernommen in BSV 2026', value: 'in_force' },
+  { title: 'Nicht übernommen', value: 'draft' },
 ]
