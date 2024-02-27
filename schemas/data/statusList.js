7674bcafba062b4cd04fe2be39140be9942564d4
mornir <jeromepott@hotmail.com>
2024-02-27 10:12:21 +0100

    update: title change requested

--- schemas/data/statusList.js
+++ schemas/data/statusList.js
@@ -1,7 +1,7 @@
 export const statusList = [
   { title: 'Entwurf', value: 'new_draft' },
   { title: 'Im Definitionsprozess', value: 'definition' },
-  { title: 'Fachliche Freigabe', value: 'approved' },
+  { title: 'Fachlich freigegeben', value: 'approved' },
   { title: 'Freigabe durch Kernausschuss', value: 'validated' },
   { title: 'Übernommen in BSV 2026', value: 'in_force' },
   { title: 'Nicht übernommen', value: 'draft' },
