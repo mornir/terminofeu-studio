5d1f7d789f3014916774c1cbf31471dcea64b380
mornir <jeromepott@hotmail.com>
2024-03-15 09:38:06 +0100

    update: add more groups

--- schemas/data/arbeitsgruppen.js
+++ schemas/data/arbeitsgruppen.js
@@ -6,4 +6,7 @@ export const ag = [
   { title: 'AG6', value: 'AG6' },
   { title: 'AG7', value: 'AG7' },
   { title: 'AG8', value: 'AG8' },
+  { title: 'AG9', value: 'AG9' },
+  { title: 'AG11', value: 'AG11' },
+  { title: 'AG12', value: 'AG12' },
 ]
