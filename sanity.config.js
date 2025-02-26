93dbe79ab2a01e5a8eb16a1646ba3256735c0a51
mornir <jeromepott@hotmail.com>
2025-02-26 10:27:19 +0100

    chore: upgrade packages

--- sanity.config.js
+++ sanity.config.js
@@ -23,6 +23,9 @@ export default defineConfig({
   title: 'Terminofeu',
   projectId: 'nipfx4rq',
   dataset: 'production',
+  releases: {
+    enabled: false
+  },
   scheduledPublishing: {
     enabled: false,
   },
